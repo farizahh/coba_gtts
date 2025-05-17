60f2146452a0e7c28dbcdbe862c32c16c2dc6430
farizahh <90184606+farizahh@users.noreply.github.com>
2025-05-17 12:37:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,12 @@ from streamlit_webrtc import webrtc_streamer
 st.title("My first Streamlit app")
 st.write("Hello, world")
 
-webrtc_streamer(key="example")
+# Tambahkan konfigurasi STUN server di sini
+webrtc_streamer(
+    key="example",
+    rtc_configuration={
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]}
+        ]
+    }
+)
