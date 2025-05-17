d48342cea22ac33225064147da7f88e9f25b1f57
farizahh <90184606+farizahh@users.noreply.github.com>
2025-05-17 12:34:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,7 @@
 import streamlit as st
-from gtts import gTTS
-import os
+from streamlit_webrtc import webrtc_streamer
 
-st.set_page_config(page_title="Text to Speech", page_icon="🔊", layout="centered")
+st.title("My first Streamlit app")
+st.write("Hello, world")
 
-st.title("🔊 Teks ke Suara Online")
-st.write("Masukkan teks di bawah, lalu dengarkan hasilnya!")
-
-# Input dari pengguna
-text = st.text_area("📝 Masukkan Teks Anda di sini", height=150)
-
-# Tombol konversi
-if st.button("🔁 Ubah Jadi Suara"):
-    if text.strip() == "":
-        st.warning("Teks tidak boleh kosong.")
-    else:
-        # Konversi teks jadi audio pakai gTTS
-        tts = gTTS(text)
-        tts.save("output.mp3")
-
-        # Tampilkan audio player
-        audio_file = open("output.mp3", "rb")
-        audio_bytes = audio_file.read()
-        st.success("✅ Berhasil dikonversi! Dengarkan di bawah:")
-        st.audio(audio_bytes, format="audio/mp3")
+webrtc_streamer(key="example")
